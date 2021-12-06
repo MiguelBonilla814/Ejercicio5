89278ad9ac7f3e1f4eb5703dcc99c7e46aea54db
miguelbonilla814 <miguelbonilla814@gmail.com>
2021-12-05 21:28:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio5/ActivityFracciones.java
@@ -0,0 +1,69 @@
+package com.example.ejercicio5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class ActivityFracciones extends AppCompatActivity {
+
+    EditText editText_ingresoFraccion;
+    ArrayList<String> fracciones;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fracciones);
+
+        editText_ingresoFraccion = findViewById(R.id.editText_fraccion);
+    }
+
+    public void IngresarFraccion(View view){
+        fracciones = new ArrayList<>();
+        String fraccion = editText_ingresoFraccion.getText().toString();
+        if (ValidadorFraccion(fraccion)){
+            fracciones.add(fraccion);
+        }
+    }
+
+    public static boolean ValidadorFraccion(String fraccion){
+        return fraccion.matches("\\d+(\\/)\\d+");
+    }
+
+    public static int[] QuiskSort(int[] arreglo, int primero, int ultimo){
+        int i,j, pivote, auxiliar;
+        i = primero;
+        j = ultimo;
+        pivote=arreglo[(primero+ultimo)/2];
+        do {
+            while (arreglo[i] < pivote){
+                i++;
+            }
+
+            while (arreglo[j] > pivote){
+                j--;
+            }
+
+            if(i<=j){
+                auxiliar = arreglo[i];
+                arreglo[i] = arreglo[j];
+                arreglo[j] = auxiliar;
+                i++;
+                j--;
+            }
+        }while(i <= j);
+
+        if(primero < j){
+            QuiskSort(arreglo, primero, j);
+        }
+        if(i < ultimo){
+            QuiskSort(arreglo, i, ultimo);
+        }
+        return arreglo;
+    }
+
+
+}
